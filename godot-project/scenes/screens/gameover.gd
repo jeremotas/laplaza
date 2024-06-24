bf62291f215052ea90108fd5b5668c6adaf51109
Jeremias Cavallero <jcavallero@quae.com.ar>
2024-06-23 22:47:27 -0300

    Separacion de botones en victoria y derrota.

--- godot-project/scenes/screens/gameover.gd
+++ godot-project/scenes/screens/gameover.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready():
-	$VBoxContainer/Reiniciar.grab_focus()
+	$MarginContainer2/VBoxContainer/Reiniciar.grab_focus()
 
 func _on_reiniciar_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/" + Global.settings.game.init_level + ".tscn")
