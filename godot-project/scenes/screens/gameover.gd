9ad87d3a0aa6c57d237e6afe8dc17f6de31decbb
Jeremias Cavallero <jcavallero@quae.com.ar>
2024-10-05 19:06:15 -0300

    Otros cambios mas
    WISHLIST y seguinos en steam
    Mejora en probabilidades de cartas
    Mejora en visual de gamewin y gameover
    Mejora visual con los outlines para el decision_time

--- godot-project/scenes/screens/gameover.gd
+++ godot-project/scenes/screens/gameover.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready():
-	$MarginContainer2/VBoxContainer/Reiniciar.grab_focus()
+	$MarginContainer2/VBoxContainer/Wishlist.grab_focus()
 
 func _on_reiniciar_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/" + Global.settings.game.init_level + ".tscn")
@@ -10,3 +10,12 @@ func _on_reiniciar_pressed():
 func _on_salir_pressed():
 	get_tree().change_scene_to_file("res://scenes/screens/inicio.tscn")
 
+
+
+func _on_wishlist_pressed():
+	OS.shell_open("http://laromero.com.ar/wishlist/")
+
+
+func _on_steam_pressed():
+	OS.shell_open("http://laromero.com.ar/wishlist/")
+	pass # Replace with function body.
