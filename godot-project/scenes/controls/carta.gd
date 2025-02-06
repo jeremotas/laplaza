f7a7dbb8144a3bf881791df34eab74d62155284f
Jeremias Cavallero <jcavallero@quae.com.ar>
2025-02-06 00:59:03 -0300

    Primeros intentos de animacion con cartas.

--- godot-project/scenes/controls/carta.gd
+++ godot-project/scenes/controls/carta.gd
@@ -3,10 +3,8 @@ extends TextureButton
 var sTipo = ""
 var sDecisionTimeMessage = ""
 var iCantidad = 0
-
 var sImagenPrincipal = ""
-var sTitulo = ""
-
+@onready var oTitulo = $Titulo
 
 var oTiposDeCartas = {
 	#{"normal": "", "selected": ""}
@@ -19,10 +17,19 @@ var oTiposDeCartas = {
 }
 #res://assets/created/carta_frente.png
 
+func _ready():
+	global_position.x = 640 - 84
+	global_position.y = 480
+
+func _process(_delta):
+	#print(global_position)
+	pass
+
 func prepare(oCartaValues):
 	sTipo = oCartaValues.tipo
 	sDecisionTimeMessage = oCartaValues.decision_message
 	iCantidad = oCartaValues.cantidad
+	oTitulo.text = oCartaValues.titulo
 	
 	# Preparo texturas del boton
 	# Preparo los textos
