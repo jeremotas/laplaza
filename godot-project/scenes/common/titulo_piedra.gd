4987134b82b866eab3376014718943550ebdd2f5
Jeremias Cavallero <jcavallero@quae.com.ar>
2026-04-03 18:43:52 -0300

    Nuevo elemento: titulo de piedra.

---
+++ godot-project/scenes/common/titulo_piedra.gd
@@ -0,0 +1,4 @@
+extends Control
+
+func set_title(sText):
+	$Label.text = sText
--- godot-project/scenes/controls/speechbubble.gd
+++ godot-project/scenes/controls/speechbubble.gd
@@ -1,11 +1,14 @@
 extends CanvasLayer
+var titulo_piedra_elemetn
 
 func _ready() -> void:
 	TranslationServer.set_locale(Global.language)
+	
+	# speech("Hola tarolas") # Test
 
 func speech(sMessage):
 	
-	$MarginContainer/RichTextLabel.text = "[center]" + tr(sMessage) + "[/center]"
+	$MarginContainer/TituloPiedra.set_title(tr(sMessage))
 	visible = true
 	$AnimationPlayer.play("title")
 	await get_tree().create_timer(3).timeout
@@ -13,8 +16,8 @@ func speech(sMessage):
 	visible = false
 	
 func advanced_speech(sMessage, time, color):
-	$MarginContainer/RichTextLabel.text = "[center]" + tr(sMessage) + "[/center]"
-	$MarginContainer/RichTextLabel.modulate = color
+	$MarginContainer/TituloPiedra.set_title(tr(sMessage))
+	$MarginContainer/TituloPiedra.modulate = color
 	visible = true
 	$AnimationPlayer.play("title")
 	await get_tree().create_timer(time).timeout
