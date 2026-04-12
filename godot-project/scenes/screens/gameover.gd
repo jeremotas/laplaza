3c88e1bb16e73aafee66c383a4f6fd0a075503c8
Jeremias Cavallero <jcavallero@quae.com.ar>
2026-04-12 11:39:19 -0300

    Victoria y derrota

--- godot-project/scenes/screens/gameover.gd
+++ godot-project/scenes/screens/gameover.gd
@@ -3,18 +3,19 @@ var tween : Tween
 
 func _ready():
 	TranslationServer.set_locale(Global.language)
-	if name == 'Victoria':
-		dar_recompensa_sobre("rojo")
-	elif Global.save_data.lagrimas_ultima_run > 1200:
-		dar_recompensa_sobre("verde")
-	elif Global.save_data.lagrimas_ultima_run > 100:
-		dar_recompensa_sobre("azul")
+	#if name == 'Victoria':
+	#	dar_recompensa_sobre("rojo")
+	#elif Global.save_data.lagrimas_ultima_run > 1200:
+	#	dar_recompensa_sobre("verde")
+	#elif Global.save_data.lagrimas_ultima_run > 100:
+	#	dar_recompensa_sobre("azul")
 		
-	$MarginContainer2/VBoxContainer/Reiniciar.grab_focus()
+	$MarginContainer2/GC/VBoxContainer/Reiniciar.grab_focus()
 		
 	var aButtons = [
-		$MarginContainer2/VBoxContainer/Reiniciar,
-		$MarginContainer2/VBoxContainer/Salir
+		$MarginContainer2/GC/Wishlist,
+		$MarginContainer2/GC/VBoxContainer/Reiniciar,
+		$MarginContainer2/GC/VBoxContainer/Salir
 	]
 	Global.prepare_buttons_menu(aButtons)
 
@@ -40,23 +41,23 @@ func dar_recompensa_sobre(sSobreType):
 	if sSobreType == "rojo":
 		Global.save_data.sobres_pendientes.rojo +=1
 		
-	show_sobrecito(sSobreType)
+	# show_sobrecito(sSobreType)
 		
 	Global.save_data.save()
 
 func show_sobrecito(sSobreType):
-	$ContenedorSobre/Sobre.type(sSobreType)
+	$MarginContainer3/ContenedorSobre/Sobre.type(sSobreType)
 	if tween and tween.is_running():
 		tween.kill()
 	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	
-	var original_pos = $ContenedorSobre.global_position
-	var final_pos = $ContenedorSobre.global_position
+	var original_pos = $MarginContainer3/ContenedorSobre.global_position
+	var final_pos = $MarginContainer3/ContenedorSobre.global_position
 	final_pos.x -= 180
 	
 	
-	tween.tween_property($ContenedorSobre, "global_position", final_pos, 1.5).set_delay(1.0)
+	tween.tween_property($MarginContainer3/ContenedorSobre, "global_position", final_pos, 1.5).set_delay(1.0)
 	await tween.finished
 	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property($ContenedorSobre, "global_position", original_pos, 1.5).set_delay(2.0)
+	tween.tween_property($MarginContainer3/ContenedorSobre, "global_position", original_pos, 1.5).set_delay(2.0)
 	await tween.finished
